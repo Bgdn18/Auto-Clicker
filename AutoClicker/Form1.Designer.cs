4563ad8ce69f632f2071b62d0e01c09d2bd7e5ef
Bogdan Khorolsky <krati18012012@gmail.com>
2025-03-11 07:16:29 -0700

    first commit

---
+++ AutoClicker/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace AutoClicker
+{
+    partial class AutoClicker
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoClicker));
+            textBoxInterval = new TextBox();
+            textBoxDuration = new TextBox();
+            buttonStart = new Button();
+            buttonStop = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // textBoxInterval
+            // 
+            textBoxInterval.Font = new Font("Comic Sans MS", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBoxInterval.Location = new Point(12, 12);
+            textBoxInterval.Name = "textBoxInterval";
+            textBoxInterval.PlaceholderText = "Speed (in milliseconds)";
+            textBoxInterval.Size = new Size(226, 26);
+            textBoxInterval.TabIndex = 0;
+            textBoxInterval.Text = "1000";
+            // 
+            // textBoxDuration
+            // 
+            textBoxDuration.Font = new Font("Comic Sans MS", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBoxDuration.Location = new Point(12, 44);
+            textBoxDuration.Name = "textBoxDuration";
+            textBoxDuration.PlaceholderText = "Time (in seconds)";
+            textBoxDuration.Size = new Size(226, 26);
+            textBoxDuration.TabIndex = 1;
+            textBoxDuration.Text = "5";
+            // 
+            // buttonStart
+            // 
+            buttonStart.Cursor = Cursors.Hand;
+            buttonStart.Font = new Font("Comic Sans MS", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            buttonStart.Location = new Point(12, 76);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(103, 54);
+            buttonStart.TabIndex = 2;
+            buttonStart.Text = "Start";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // buttonStop
+            // 
+            buttonStop.Cursor = Cursors.Hand;
+            buttonStop.Enabled = false;
+            buttonStop.Font = new Font("Comic Sans MS", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            buttonStop.Location = new Point(135, 76);
+            buttonStop.Name = "buttonStop";
+            buttonStop.Size = new Size(103, 54);
+            buttonStop.TabIndex = 3;
+            buttonStop.Text = "Stop";
+            buttonStop.UseVisualStyleBackColor = true;
+            buttonStop.Click += buttonStop_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Comic Sans MS", 11.25F, FontStyle.Bold | FontStyle.Italic);
+            label1.Location = new Point(12, 133);
+            label1.Name = "label1";
+            label1.Size = new Size(28, 22);
+            label1.TabIndex = 4;
+            label1.Text = "F6";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Comic Sans MS", 11.25F, FontStyle.Bold | FontStyle.Italic);
+            label2.Location = new Point(135, 133);
+            label2.Name = "label2";
+            label2.Size = new Size(28, 22);
+            label2.TabIndex = 5;
+            label2.Text = "F7";
+            // 
+            // AutoClicker
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(250, 165);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(buttonStop);
+            Controls.Add(buttonStart);
+            Controls.Add(textBoxDuration);
+            Controls.Add(textBoxInterval);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "AutoClicker";
+            Text = "Auto Clicker";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBoxInterval;
+        private TextBox textBoxDuration;
+        private Button buttonStart;
+        private Button buttonStop;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ AutoClicker/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace AutoClicker
+{
+    public partial class AutoClicker : Form
+    {
+        //WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI 
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        //WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI WinAPI 
+
+        //Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse 
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse Mouse 
+
+        //MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x0002;
+        private const uint MOUSEEVENTF_LEFTUP = 0x0004;
+        //MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT MOUSE EVENT
+
+        //HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS 
+        private const int HOTKEY_ID_START = 1; // Идентификатор для F6
+        private const int HOTKEY_ID_STOP = 2; // Идентификатор для F7
+        //HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS HOT KEYS 
+
+        //CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK 
+        private System.Windows.Forms.Timer clickTimer; // Таймер для кликов
+        private System.Windows.Forms.Timer durationTimer; // Таймер для ограничения времени работы
+        private int clickInterval; // Интервал кликов в миллисекундах
+        private int duration; // Время работы в секундах
+        //CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK CLICK 
+
+        public AutoClicker()
+        {
+            InitializeComponent();
+            clickTimer = new System.Windows.Forms.Timer();
+            durationTimer = new System.Windows.Forms.Timer();
+            clickTimer.Tick += ClickTimer_Tick!;
+            durationTimer.Tick += DurationTimer_Tick!;
+
+            // Подключаем обработчик события KeyDown
+            this.KeyDown += Form1_KeyDown!;
+            this.KeyPreview = true; // Разрешаем форме перехватывать события клавиш
+
+            //REGISTER HOT KEYS REGISTER HOT KEYS REGISTER HOT KEYS REGISTER HOT KEYS REGISTER HOT KEYS 
+            RegisterHotKey(this.Handle, HOTKEY_ID_START, 0, (int)Keys.F6); // F6
+            RegisterHotKey(this.Handle, HOTKEY_ID_STOP, 0, (int)Keys.F7); // F7
+            //REGISTER HOT KEYS REGISTER HOT KEYS REGISTER HOT KEYS REGISTER HOT KEYS REGISTER HOT KEYS 
+
+            //FORM STYLE FORM STYLE FORM STYLE FORM STYLE FORM STYLE FORM STYLE FORM STYLE FORM STYLE 
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            //FORM STYLE FORM STYLE FORM STYLE FORM STYLE FORM STYLE FORM STYLE FORM STYLE FORM STYLE 
+        }
+
+        // Переопределяем метод WndProc для перехвата сообщений
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_HOTKEY = 0x0312; // Сообщение о нажатии горячей клавиши
+
+            if (m.Msg == WM_HOTKEY)
+            {
+                int id = m.WParam.ToInt32(); // Получаем идентификатор горячей клавиши
+
+                switch (id)
+                {
+                    case HOTKEY_ID_START:
+                        buttonStart_Click(null!, null!); // Запуск автокликера
+                        break;
+                    case HOTKEY_ID_STOP:
+                        buttonStop_Click(null!, null!); // Остановка автокликера
+                        break;
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        //KEY DOWN KEY DOWN KEY DOWN KEY DOWN KEY DOWN KEY DOWN KEY DOWN KEY DOWN KEY DOWN KEY DOWN 
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Если нажата F6, запускаем автокликер
+            if (e.KeyCode == Keys.F6)
+            {
+                buttonStart_Click(null!, null!); // Вызываем метод старта
+            }
+
+            // Если нажата F7, останавливаем автокликер
+            if (e.KeyCode == Keys.F7)
+            {
+                buttonStop_Click(null!, null!); // Вызываем метод остановки
+            }
+        }
+        //KEY DOWN KEY DOWN KEY DOWN KEY DOWN KEY DOWN KEY DOWN KEY DOWN KEY DOWN KEY DOWN KEY DOWN 
+
+        //START CLICK START CLICK START CLICK START CLICK START CLICK START CLICK START CLICK START CLICK 
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            // Получаем интервал кликов из textBoxInterval
+            if (!int.TryParse(textBoxInterval.Text, out clickInterval) || clickInterval <= 0)
+            {
+                MessageBox.Show("Введите интервал в миллисекундах!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Получаем время работы из textBoxDuration
+            if (!int.TryParse(textBoxDuration.Text, out duration) || duration <= 0)
+            {
+                MessageBox.Show("Введите время работы в секундах!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            //START CLICK START CLICK START CLICK START CLICK START CLICK START CLICK START CLICK START CLICK 
+
+
+
+        //TIMER START TIMER START TIMER START TIMER START TIMER START TIMER START TIMER START TIMER START 
+            clickTimer.Interval = clickInterval;
+            clickTimer.Start();
+
+            durationTimer.Interval = duration * 1000; // Переводим секунды в миллисекунды
+            durationTimer.Start();
+
+            // Блокируем кнопку "Старт" и разблокируем "Стоп"
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+
+            // Отладочный вывод
+            Console.WriteLine("Автокликер запущен!");
+        }
+        //TIMER START TIMER START TIMER START TIMER START TIMER START TIMER START TIMER START TIMER START 
+
+
+
+        //STOP CLICK STOP CLICK STOP CLICK STOP CLICK STOP CLICK STOP CLICK STOP CLICK STOP CLICK STOP CLICK 
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            // Останавливаем таймеры
+            clickTimer.Stop();
+            durationTimer.Stop();
+
+            // Разблокируем кнопку "Старт" и блокируем "Стоп"
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+
+            // Отладочный вывод
+            Console.WriteLine("Автокликер остановлен!");
+        }
+        //STOP CLICK STOP CLICK STOP CLICK STOP CLICK STOP CLICK STOP CLICK STOP CLICK STOP CLICK STOP CLICK 
+
+        //CLICK TIMER CLICK TIMER CLICK TIMER CLICK TIMER CLICK TIMER CLICK TIMER CLICK TIMER CLICK TIMER 
+        private void ClickTimer_Tick(object sender, EventArgs e)
+        {
+            // Выполняем клик
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+            // Отладочный вывод
+            Console.WriteLine("Клик выполнен!");
+        }
+        //CLICK TIMER CLICK TIMER CLICK TIMER CLICK TIMER CLICK TIMER CLICK TIMER CLICK TIMER CLICK TIMER 
+
+        //DURATION TICK DURATION TICK DURATION TICK DURATION TICK DURATION TICK DURATION TICK DURATION TICK 
+        private void DurationTimer_Tick(object sender, EventArgs e)
+        {
+            // Останавливаем таймеры
+            clickTimer.Stop();
+            durationTimer.Stop();
+
+            // Разблокируем кнопку "Старт" и блокируем "Стоп"
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+
+            // Отладочный вывод
+            Console.WriteLine("Автокликер остановлен по истечении времени!");
+
+            MessageBox.Show("Автокликер остановлен по истечении времени.", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        //DURATION TICK DURATION TICK DURATION TICK DURATION TICK DURATION TICK DURATION TICK DURATION TICK 
+
+        //CANCEL REGISTER CLICK___ CANCEL REGISTER CLICK___ CANCEL REGISTER CLICK___ CANCEL REGISTER CLICK___ 
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_ID_START); // Отменяем F6
+            UnregisterHotKey(this.Handle, HOTKEY_ID_STOP); // Отменяем F7
+            base.OnFormClosed(e);
+        }
+        //CANCEL REGISTER CLICK___ CANCEL REGISTER CLICK___ CANCEL REGISTER CLICK___ CANCEL REGISTER CLICK___ 
+
+    }
+}
